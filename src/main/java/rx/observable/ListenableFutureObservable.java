ad5ad009bb6d42dc109ed1401918aa1d4c57df0e
George Campbell <gscampbell@netflix.com>
2015-06-04 23:58:58 -0700

    put the copyright back in

--- src/main/java/rx/observable/ListenableFutureObservable.java
+++ src/main/java/rx/observable/ListenableFutureObservable.java
@@ -1,14 +1,33 @@
+/**
+ * Copyright 2014 Netflix, Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package rx.observable;
 
-import java.util.concurrent.Executor;
+import com.google.common.util.concurrent.AbstractFuture;
+import com.google.common.util.concurrent.ListenableFuture;
 
-import rx.*;
+import rx.Observable;
 import rx.Observable.OnSubscribe;
-import rx.Scheduler.Worker;
 import rx.functions.Action0;
 import rx.internal.operators.SingleDelayedProducer;
+import rx.Observer;
+import rx.Scheduler;
+import rx.Scheduler.Worker;
+import rx.Subscriber;
 
-import com.google.common.util.concurrent.*;
+import java.util.concurrent.Executor;
 
 public class ListenableFutureObservable {
 
